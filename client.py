265a21766a8bed5137690f28b972a70a4d89ba5f
Mateusz Wiza <43853451+mateuszwiza@users.noreply.github.com>
2020-12-28 16:44:49 +0100

    Step 1 almost done

--- client.py
+++ client.py
@@ -1,15 +1,13 @@
-import base64
 import json
 import socket
 
-
-# This client opens up a socket connection with the server, but only if the server program is currently running
 import cryptography
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
+import base64
 
-
+#This client opens up a socket connection with the server, but only if the server program is currently running
 def reading(file):
     my_config_file1 = open(file)
     my_config_file1 = my_config_file1.read()
@@ -17,17 +15,23 @@ def reading(file):
     return object1
 
 
-client1 = reading('data/config_1.json')
-idx = client1['person']['id']
-firstName = client1['person']['name']
-publicKey = client1['person']['keys']['public']
+client_data = reading('data/config_1.json')
+try:
+    log = reading('data/client_log.json')
+except:
+    log = {}
+    log['sending'] = []
+    log['receiving'] = []
+idx = client_data['person']['id']
+firstName = client_data['person']['name']
+publicKey = client_data['person']['keys']['public']
 print("Client 1 ID: ", idx)
 print("Name:", firstName)
 print("Public key:", publicKey)
 print("-----------------")
 
-host = client1['server']['ip']
-port = int(client1['server']['port'])
+host = client_data['server']['ip']
+port = int(client_data['server']['port'])
 
 def encrypt(message, key):
     digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
@@ -39,7 +43,7 @@ def encrypt(message, key):
 
 def decrypt(message):
     digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
-    digest.update(publicKey)
+    digest.update(publicKey.encode())
     key_ready = Fernet(base64.urlsafe_b64encode(digest.finalize()))
     
     return key_ready.decrypt(message)
@@ -58,22 +62,35 @@ print(Welcome.decode('utf-8'))
 # register
 ClientSocket.send(str.encode(idx))
 ClientSocket.send(str.encode(firstName))
+ClientSocket.send(str.encode(publicKey))
+number = str(len(client_data['actions']))
+ClientSocket.send(str.encode(number))
 Result = ClientSocket.recv(1024)
 print(Result.decode('utf-8'))
 
 # send messages
 def sendMessages(client_data):
     for action in client_data['actions']:
-        substr = action.split('[')
-        adres = substr[1].split(']')[0]
-        #get public key
-        ClientSocket.send(str.encode(adres))
-        key = ClientSocket.recv(1024)
+    substr = action.split('[')
+    adres = substr[1].split(']')[0]
+    #get public key
+    ClientSocket.send(str.encode(adres))
+    
+    key = ClientSocket.recv(1024)
+    if key == 'not found':
+        raise Exception('recipient not found')
+    else:
         #encrypt message
         message = substr[2].split(']')[0]
+        print('sending', message, 'to', adres)
+        #log message
+        log['sending'].append({'from': firstName, 'to': adres, 'message': str(message)})
+        with open('data/client_log.json', 'w') as outfile:
+            json.dump(log, outfile)
+            
         encrypted = encrypt(message, key)
         #send message
-        ClientSocket.send(str.encode(adres))
+        ClientSocket.send(str.encode(str(encrypted))
 
 try:
     sendMessages(client1)
@@ -88,11 +105,13 @@ except socket.error as e:
 # listen for incoming messages
 while True:
     try:
-        incoming = ClientSocket.recv(1024).decode('utf-8')
-        # print(decrypt(incoming))
-        print('you got a message:' + incoming)
+        incoming = decrypt(ClientSocket.recv(1024))
+        print(incoming)
+        #log message
+        log['receiving'].append({'by': firstName, 'message': str(incoming)})
+        with open('data/client_log.json', 'w') as outfile:
+            json.dump(log, outfile)
     except:
         print('no incoming messages')
-
 
 ClientSocket.close()
