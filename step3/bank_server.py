a84c1a635a729e6760b89f032ecb0819ac2388fa
Mateusz Wiza <43853451+mateuszwiza@users.noreply.github.com>
2021-01-02 20:14:43 +0100

    Step 3 done

--- step3/bank_server.py
+++ step3/bank_server.py
@@ -2,15 +2,17 @@ import socket
 import json
 from _thread import *
 import requests
+import time
 
 def reading(file):
     my_config_file1 = open(file)
     my_config_file1 = my_config_file1.read()
     object1 = json.loads(my_config_file1)
     return object1
-    
+
 credentials = reading('data/bank_credentials.json')
 transactions = []
+statuses = []
 
 try:
     log = reading('data/bankserver_log.json')
@@ -18,6 +20,7 @@ except:
     log = {}
     log['transactions'] = []
     
+
 # a function that handles requests from the individual client by a thread
 # threaded_client() connects to each individual client on the different address given by the server
 def threaded_client(connection):
@@ -28,7 +31,6 @@ def threaded_client(connection):
     # REGISTRATION
     try:
         login_info = connection.recv(1024).decode('utf-8')
-        print('login info:', login_info)
         client_id = login_info.split('#')[0]
         name = login_info.split('#')[1]
         accountNumber = login_info.split('#')[2]
@@ -53,7 +55,7 @@ def threaded_client(connection):
         return False
 
     # TRANSACTIONS
-    global transactions
+    global transactions, statuses
 
     for i in range(int(number)):
         action = connection.recv(1024).decode('utf-8')
@@ -67,9 +69,22 @@ def threaded_client(connection):
     confirmation = 'The transaction requests have been sent to the bank!'
     connection.send(str.encode(confirmation))
     
+    time.sleep(1)
+    
+    # SEND TRANSACTION STATUS TO CLIENT
+    while True:
+        for status in statuses:
+            transaction_id = status.split('#')[0]
+            if transaction_id[0:4] == client_id:
+                message = status.split('#')[1]
+                tosend = 'Transaction ' +  transaction_id + ': ' + message
+                connection.send(str.encode(tosend))
+                statuses.remove(status)
+                time.sleep(1)
+    
     #connection.close()
     return True
-    
+
 def threaded_bank(connection):
     welcome = 'Welcome to the Server\n'
     connection.send(str.encode(welcome))
@@ -78,7 +93,6 @@ def threaded_bank(connection):
     # REGISTRATION
     try:
         login_info = connection.recv(1024).decode('utf-8')
-        print('login info:', login_info)
         bank_id = login_info.split('#')[0]
         name = login_info.split('#')[1]
     except:
@@ -88,11 +102,10 @@ def threaded_bank(connection):
         return False
 
     for user in credentials:
-        if name == str(user['user_name']) and bank_id == int(user['user_id']):
+        if name == str(user['user_name']) or bank_id == int(user['user_id']):
             response = 'Registration successful'
             connection.send(str.encode(response))
             registered = True
-            return True
     
     if not registered:
         response = 'Registration error: incorrect credentials'
@@ -101,13 +114,20 @@ def threaded_bank(connection):
         return False
     
     # SEND TRANSACTIONS TO BANK
-    global transactions
+    global transactions, statuses
     
     while True:
-        for i in range(len(transactions)):
-            connection.send(str.encode(transactions[i]))
+        for command in transactions:
+            connection.send(str.encode(command))
+            print('sending to bank', command)
+            transactions.remove(command)
             
-def server():
+            #get status
+            status = connection.recv(1024).decode('utf-8')
+            print(status)
+            statuses.append(status)
+            
+  def server():
     ServerSocketClient = socket.socket()
     ServerSocketBank = socket.socket()
     # declare host and port on which we need to communicate with clients
@@ -126,25 +146,32 @@ def server():
 
     # use a while loop to make it run Server endlessly until we manually stop the Server
     while True:
+        ServerSocketBank.listen(1)
         ServerSocketClient.listen(1)
-        ServerSocketBank.listen(2)
-        if ServerSocketClient.accept():
+        
+        try:
             Client, address = ServerSocketClient.accept()
             print('Connected to client: ' + address[0] + ':' + str(address[1]))
 
             start_new_thread(threaded_client, (Client,))
             ThreadCountClient += 1
             print('Client Thread Number: ' + str(ThreadCountClient))
+            print('------------------')
+        except:
+            pass
         
-        if ServerSocketBank.accept():
+        try:
             Bank, address = ServerSocketBank.accept()
             print('Connected to bank: ' + address[0] + ':' + str(address[1]))
 
             start_new_thread(threaded_bank, (Bank,))
             ThreadCountBank += 1
             print('Bank Thread Number: ' + str(ThreadCountBank))
+            print('------------------')
+        except:
+            pass
 
-    ServerSocket.close()
+    ServerSocket.close()  
     
-if __name__ == '__main__':
-    server()
+    if __name__ == '__main__':
+        server()
